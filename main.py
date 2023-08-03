e8e90c89dfb221ec8409bccc390819e16e12affb
juancamacho26 <117555244+juancamacho26@users.noreply.github.com>
2023-08-02 19:01:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# mi primer archivo python
+#Ejercicio 9
+'''
+inversion = input ("Escriba la cantidad a invertir: ")
+inv = int(inversion)
+
+intmanual = input ("Escriba el interes manual: ")
+
+intm = int(intmanual)
+
+numanios = input ("Escriba el numero de años: ")
+float (numanios)
+
+capitalobtenido = inv(intm/100+1)**numanios
+
+print ("Este fue el capital obtenido")
+
+print (capitalobtenido)'''
+
+#ejercicio 10
